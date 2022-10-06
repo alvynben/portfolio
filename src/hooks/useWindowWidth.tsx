f9f01d2a4f5c6482eebbbe4cd79820f8f8d0ecb3
alvynben <alvynben@gmail.com>
2022-10-06 13:48:33 +0800

    feat: create new hook to get windowWidth

---
+++ src/hooks/useWindowWidth.tsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+export default function useWindowWidth() {
+  const [width, setWidth] = useState(window.innerWidth);
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  return width;
+}
