109e03885822ac82d4b708015fca981df5722948
alvynben <alvynben@gmail.com>
2022-10-08 11:27:23 +0800

    feat: increase readability for socialMediaBar

--- src/components/SocialMediaBar/SocialMediaBar.tsx
+++ src/components/SocialMediaBar/SocialMediaBar.tsx
@@ -1,28 +1,49 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
-import React from "react";
+import React, { useState } from "react";
 import "./SocialMediaBar.css";
-import cvPDF from 'assets/CV.pdf';
+import cvPDF from "assets/CV.pdf";
+
+
 
 function SocialMediaBar() {
+  const [mouseInside, setMouseInside] = useState(false);
+
+  const SocialMediaText = (text: string) => (
+    <p style={{fontSize: '12px', margin: 0, display: mouseInside ? 'block' : 'none'}}>{text}</p>
+  )
   return (
-    <div className="icon-bar">
-      <div onClick={() => {
+    <div onMouseEnter={() => setMouseInside(true)} onMouseLeave={() => setMouseInside(false)} className="icon-bar">
+      <div
+        onClick={() => {
           window.open("https://linkedin.com/in/alvinbenabraham", "_blank");
-        }} className="linkedin">
+        }}
+        className="linkedin"
+      >
         <FontAwesomeIcon icon={faLinkedin} />
+        {SocialMediaText('Linkedin')}
       </div>
-      <div onClick={() => {
+      <div
+        onClick={() => {
           window.open("https://github.com/alvynben", "_blank");
-        }} className="github">
+        }}
+        className="github"
+      >
         <FontAwesomeIcon icon={faGithub} />
+        {SocialMediaText('GitHub')}
       </div>
-      <div className="resume">
-      <a style={{textDecoration: 'none', color: '#fff'}} href={cvPDF} target="_blank" rel="noreferrer">
+      <a
+        style={{ textDecoration: "none", color: "#fff" }}
+        href={cvPDF}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <div className="resume">
           <FontAwesomeIcon icon={faFilePdf} />
-        </a>
-      </div>
+          {SocialMediaText('Resume')}
+        </div>
+      </a>
     </div>
   );
 }
